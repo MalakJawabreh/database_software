d77bb34d06457853760d1f6edc6d999f5d58e064
malak jawabreh <s12028161@stu.najah.edu>
2024-11-22 21:53:44 +0200

    update the app.js

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const body_parser = require('body-parser');
 const userRouter  = require ('./routers/user.routers');
 
+
 const app=express();
 app.use(body_parser.json());
-
+app.use(cors());
 app.use('/',userRouter);
 //aya awwad test new!
 module.exports=app;
